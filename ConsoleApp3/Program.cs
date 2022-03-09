1710ad708adad3a3ebf5c9ddce39afd7bfde5433
Murtaza Behzad <murtaza.b@gso.schule.koeln>
2022-03-09 10:35:08 +0100

    Projektdateien hinzufügen.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Note = 0;
+            int anzahlschueler = 20;
+            
+            int[] noten = new int[anzahlschueler];
+            
+            int Notensumme = 0;
+            
+            for (int anzahlSchul = 0; anzahlSchul < anzahlschueler; anzahlSchul++)
+            {
+                int Anzah = 0;
+
+                
+                Console.WriteLine("Schüler {0}", anzahlSchul);
+                
+
+
+                Note = Convert.ToInt32(Console.ReadLine());
+                noten[anzahlSchul] = Note;
+
+                noten[anzahlSchul] = Note;
+                Notensumme = Notensumme + Note;
+                
+            }
+
+            float NotenDurch = 0f;
+            NotenDurch = Notensumme / anzahlschueler;
+            Console.WriteLine("Der Noten durchschnitt liegt bei:" + NotenDurch);
+             //deniz kartal und murtaza behzad
+        }
+    }
+}
