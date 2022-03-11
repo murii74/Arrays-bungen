08f4d683c1594eebc8bc3dc2285b0392ed6051e6
Murtaza Behzad <murtaza.b@gso.schule.koeln>
2022-03-11 12:57:16 +0100

    BIG BUBBLE B

---
+++ AUTO SORT/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace AUTO_SORT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 696969, 2000, 720800, 80000000 };
+            Array.Sort(array);
+            Array.Reverse(array);
+           
+            Console.Write(array[0]);
+            Console.Write(" ");
+            Console.Write(array[1]);
+            Console.Write(" ");
+            Console.Write(array[2]);
+            Console.Write(" ");
+            Console.Write(array[3]);
+            foreach (int value in array)
+                Console.Write(value + " ");
+            
+
+
+
+        }
+
+    }
+}
