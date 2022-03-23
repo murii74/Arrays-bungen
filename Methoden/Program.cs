a28da10699746bdda745db258208737f29b77f93
Murtaza Behzad <murtaza.b@gso.schule.koeln>
2022-03-23 12:22:24 +0100

    Big Juicer

---
+++ Methoden/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Methoden
+{
+    class Program
+    {  
+            static void Main(string[] args)
+            {
+
+                Console.WriteLine("Wie oft soll das wort geschrieben werden");
+                int durchlauf = Convert.ToInt32(Console.ReadLine());
+                Console.Clear();
+
+                Console.WriteLine("Geben sie bitte ihr Wort ein");
+                string Text = Console.ReadLine();
+                Console.Clear();
+
+                int anzahl = 0;
+                anzahl = ausgabeTextMalX(durchlauf, Text);
+
+                Console.Write("\nAnzahl der Buchstaben: {0}", anzahl);
+            }
+
+            static int ausgabeTextMalX(int durchlauf, string Text)
+            {
+                for (int i = 0; i < durchlauf; i++)
+                {
+                    Console.WriteLine(Text);
+                }
+            return Text.Length * durchlauf;
+            }
+    }
+
+}
